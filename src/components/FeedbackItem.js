ca2747ae0a1e4f8a009d5699f1d6976da148bf9b
Shubham Chaubey <shubhamchaubey2211@gmail.com>
2022-11-24 23:06:52 +0530

    modified:   src/App.css 	modified:   src/components/FeedbackItem.js 	new file:   src/components/shared/Card.js

--- src/components/FeedbackItem.js
+++ src/components/FeedbackItem.js
@@ -1,9 +1,14 @@
+import Card from "./shared/Card"
+
+
 function FeedbackItem({item}) {
   return (
-    <div className='card'>
+    <Card>
+    <div>
         <div className='num-display'>{item.rating}</div>
         <div className='text-display'>{item.text}</div>
     </div>
+    </Card>
   )
 }
 export default FeedbackItem
