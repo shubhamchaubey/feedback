d3a462d3b0181b7a3dfdb0a327d614dd61a6fbea
Shubham Chaubey <shubhamchaubey2211@gmail.com>
2022-11-27 12:16:25 +0530

    modified:   src/App.js 	modified:   src/components/FeedbackForm.js 	modified:   src/components/FeedbackList.js     Completed the function to add the feedback

--- src/App.js
+++ src/App.js
@@ -5,20 +5,26 @@ import FeedbackData from './data/FeedbackData';
 import Header from './components/Header';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
+import {v4 as uuidv4} from 'uuid'
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData)
+
   const deleteFeedback = (id) => {
     if(window.confirm('Are you sure that you want to delete it?')){
       setFeedback(feedback.filter((item)=>item.id !== id))
     }
-    
   }
+
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4()
+    setFeedback([newFeedback, ...feedback])
+  } 
   return (
     <>
     <Header text = "Feedback UI" />
     <div className='container'>
-      <FeedbackForm />
+      <FeedbackForm handleAdd={addFeedback}/>
       <FeedbackStats feedback={feedback}/>
       <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
     </div>
--- src/components/FeedbackList.js
+++ src/components/FeedbackList.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import FeedbackItem from './FeedbackItem'
 import PropTypes from 'prop-types'
+import { motion, AnimatePresence } from 'framer-motion'
 
 function FeedbackList({feedback, handleDelete}) {
   if (!feedback || feedback.length===0){
     return <h3>No feedback yet!</h3>
   }
+
   return (
     <div className='feedback-list'>
       {feedback.map((item) => (
@@ -17,12 +19,25 @@ function FeedbackList({feedback, handleDelete}) {
       ))}
     </div>
   )
+
+  //With no animation
+  // return (
+  //   <div className='feedback-list'>
+  //     {feedback.map((item) => (
+  //       <FeedbackItem 
+  //         key={item.id} 
+  //         item={item} 
+  //         handleDelete = {handleDelete}
+  //       />
+  //     ))}
+  //   </div>
+  // )
 }
 
 FeedbackList.propTypes = {
   feedback: PropTypes.arrayOf(
     PropTypes.shape({
-      id: PropTypes.number.isRequired,
+      // id: PropTypes.string.isRequired,
       text: PropTypes.string.isRequired,
       rating: PropTypes.number.isRequired,
     })
